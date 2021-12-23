73888aafb0d410fccbf3f503f0d3243d93fb3bcb
Ferdianqbl <muhammadferdianiqbal@gmail.com>
2021-12-23 17:33:42 +0700

    add mostPicked component, refMostPicked, and update landingPage component

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import Navbar from 'parts/Navbar';
 import Hero from 'parts/Hero';
 import landingPage from '../json/landingPage.json';
+import MostPicked from 'parts/MostPicked';
 
 const LandingPage = () => {
   const location = useLocation();
   // console.log(location);
   document.title = "Vacastay | Home";
 
+  const refMostPicked = useRef();
+
   return (
     <>
       <Navbar location={location.pathname}></Navbar>
-      <Hero data={landingPage.hero} />
+      <Hero data={landingPage.hero} refMostPicked={refMostPicked} />
+      <MostPicked data={landingPage.mostPicked} refMostPicked={refMostPicked} />
     </>
   )
 }
---
+++ src/parts/MostPicked.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Button from "../components/Button/Button";
+
+const MostPicked = (props) => {
+  return (
+    <section className="most-picked" id="most-picked" ref={props.refMostPicked}>
+      <div className="container">
+        <h3 className="section-heading mb-3">Most Picked</h3>
+
+        <div className="container-grid sm">
+          {props.data.map((item, index) => {
+            return (
+              <div
+                className={`item column-4 ${index === 0 ? "row-2" : "row-1"}`}
+                key={`most-picked${index + 1}`}
+              >
+                <div className="card card-featured">
+                  <div className="tag">
+                    ${item.price}
+                    <span className="tag-price"> per {item.unit}</span>
+                  </div>
+
+                  <figure className="img-wrapper">
+                    <img
+                      src={item.imageUrl}
+                      alt={item.name}
+                      className="img-cover"
+                    />
+                  </figure>
+
+                  <div className="meta-wrapper">
+                    <Button
+                      type="link"
+                      className="streched-link d-block text-white"
+                      href={`/properties/${item._id}`}
+                    >
+                      <h5>{item.name}</h5>
+                    </Button>
+                    <span>{item.city}, {item.country}</span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MostPicked;
