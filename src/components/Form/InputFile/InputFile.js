d7fe7b225b0d878a5dada6002f394251f5aaaecc
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-01-05 11:32:45 +0700

    add inputFile and inputText components

---
+++ src/components/Form/InputFile/InputFile.js
@@ -0,0 +1,73 @@
+import React, { useRef, useState } from "react";
+import "./InputFile.scss";
+import propTypes from "prop-types";
+
+const InputFile = (props) => {
+  const [fileName, setFileName] = useState("");
+  const {
+    placeholder,
+    name,
+    accept,
+    prepend,
+    append,
+    outerClassName,
+    inputClassName,
+    onChange,
+  } = props;
+
+  const refInputFile = useRef(null);
+
+  return (
+    <div className={["input-text mb-3", outerClassName].join(" ")}>
+      <div className="input-group">
+        {prepend && (
+          <div className="input-group-prepend bg-gray-900">
+            <span className="input-group-text">{prepend}</span>
+          </div>
+        )}
+
+        {/* main input */}
+        <input
+          accept={accept}
+          ref={refInputFile}
+          name={name}
+          className="d-none"
+          type="file"
+          value={FileName}
+          onChange={onChange}
+        />
+
+        {/* input masking ==> to show for user */}
+        <input
+          onClick={() => refInputFile.current.click()}
+          defaultValue={fileName}
+          placeholder={placeholder}
+          className={["form-control", inputClassName].join(" ")}
+        />
+        {append && (
+          <div className="input-group-append bg-gray-900">
+            <span className="input-group-text">{append}</span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+InputFile.defaultProps = {
+  placeholder: "Browse a file...",
+};
+
+InputFile.propTypes = {
+  name: propTypes.string.isRequired,
+  accept: propTypes.string.isRequired,
+  value: propTypes.string.isRequired,
+  onChange: propTypes.func.isRequired,
+  prepend: propTypes.oneOfType([propTypes.number, propTypes.string]),
+  append: propTypes.oneOfType([propTypes.number, propTypes.string]),
+  placeholder: propTypes.string,
+  outerClassName: propTypes.string,
+  inputClassName: propTypes.string,
+};
+
+export default InputFile;
---
+++ src/components/Form/InputText/InputText.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import "./InputText.scss";
+import propTypes from "prop-types";
+
+const InputText = () => {
+  const {
+    value,
+    type,
+    placeholder,
+    name,
+    prepend,
+    append,
+    outerClassName,
+    inputClassName,
+    errorResponse,
+  } = props;
+
+  const [hasError, setHasError] = useState(null);
+  let pattern = "";
+  if (type === "email") pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (type === "tel") pattern = "[0-9]*";
+
+  const onChange = (e) => {
+    const target = {
+      target: {
+        name,
+        value: e.target.value,
+      },
+    };
+
+    if (type === "email") {
+      if (!pattern.test(e.target.value)) setHasError(errorResponse);
+      else setHasError(null);
+    }
+
+    if (type === "tel") {
+      if (e.target.validity.valid) props.onChange(target);
+      else props.onChange(target);
+    }
+  };
+
+  return (
+    <div className={["input-text mb-3", outerClassName].join(" ")}>
+      <div className="input-group">
+        {prepend && (
+          <div className="input-group-prepend bg-gray-900">
+            <span className="input-group-text">{prepend}</span>
+          </div>
+        )}
+        <input
+          name={name}
+          type={type}
+          pattern={pattern}
+          className={["form-control", inputClassName].join(" ")}
+          value={value}
+          placeholder={placeholder}
+          onChange={onChange}
+        />
+        {append && (
+          <div className="input-group-append bg-gray-900">
+            <span className="input-group-text">{append}</span>
+          </div>
+        )}
+      </div>
+      {hasError && <span className="error-helper">{hasError}</span>}
+    </div>
+  );
+};
+
+InputText.defaultProps = {
+  type: "text",
+  pattern: "",
+  placeholder: "please type here...",
+  errorResponse: "Please match the requested format.",
+};
+
+InputText.propTypes = {
+  name: propTypes.string.isRequired,
+  value: propTypes.oneOfType([propTypes.number, propTypes.string]).isRequired,
+  onChange: propTypes.func.isRequired,
+  prepend: propTypes.oneOfType([propTypes.number, propTypes.string]),
+  append: propTypes.oneOfType([propTypes.number, propTypes.string]),
+  type: propTypes.string,
+  placeholder: propTypes.string,
+  outerClassName: propTypes.string,
+  inputClassName: propTypes.string,
+};
+
+export default InputText;
