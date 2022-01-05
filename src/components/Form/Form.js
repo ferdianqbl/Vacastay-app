49a486d8e1d49ac959258f81eadc56ffb70e0467
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-01-05 12:44:35 +0700

    create first step booking information

---
+++ src/components/Form/Form.js
@@ -0,0 +1,4 @@
+export { InputDate } from "./InputDate/InputDate";
+export { InputFile } from "./InputFile/InputFile";
+export { InputNumber } from "./InputNumber/InputNumber";
+export { InputText } from "./InputText/InputText";
